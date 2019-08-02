15ab6a93ba150b893d517d057dcb83b0e560547d
Chen Bai <chenbai@ufl.edu>
2019-08-02 09:19:20 -0400

    add demographics by hospital admission

--- already_done/vitals_labs_demographics/code/demographics.sql
+++ already_done/vitals_labs_demographics/code/demographics.sql
@@ -25,3 +25,26 @@ left join death_after_a_month
 on death_after_a_month.hadm_id = adm.hadm_id
 left join death_after_a_year
 on death_after_a_year.hadm_id = adm.hadm_id;
+
+
+-- demographics on hospital stay level
+select pat.subject_id,
+	   adm.hadm_id,
+	   pat.gender, 					-- 1
+	   adm.ethnicity, 				-- 2
+	   case 
+      		when round( ( cast(adm.admittime as date) - cast(pat.dob as date) ) / 365.242 , 2 ) >= 150 then 91.4
+      		else round( ( cast(adm.admittime as date) - cast(pat.dob as date) ) / 365.242 , 2 )
+       end as age, 					-- 3
+       bmi.bmi,						-- 4
+       adm.hospital_expire_flag,
+       death_after_a_month.expire_flag as death_after_a_month
+	   from patients pat
+inner join admissions adm
+on pat.subject_id = adm.subject_id
+left join bmi_hadm bmi
+on adm.hadm_id = bmi.hadm_id
+left join death_after_a_month
+on death_after_a_month.hadm_id = adm.hadm_id
+left join death_after_a_year
+on death_after_a_year.hadm_id = adm.hadm_id;
