ae2816e27b3b452ca0376f296d914bcb6c098900
Chen Bai <chenbai@ufl.edu>
2019-08-02 08:00:50 -0400

    add hospital_expire_flag

--- already_done/chapter18/code/deResult.sql
+++ already_done/chapter18/code/deResult.sql
@@ -1,17 +1,23 @@
 -- Generate final result for chapter18
+truncate table combined_tbl;
+
 select adult.subject_id, 
-	   adult.hadm_id,
 	   adult.gender,
 	   adult.ethnicity, 
 	   adult.admittime,
+	   adult.hospital_expire_flag,
 	   case 
 	   	   when adult.age >= 300 then 91.4 -- median of age > 89
 	   	   else adult.age
 	   end as age,
 	   f.first_careunit as location,
-	   tbl.*
+	   tbl.*,
+	   bmi.bmi
 	   from adult_info adult
 inner join combined_tbl tbl
 on adult.hadm_id = tbl.hadm_id
 inner join first_careunit f
-on adult.hadm_id = f.hadm_id;
+on adult.hadm_id = f.hadm_id
+left join bmi_hadm bmi
+on bmi.hadm_id = adult.hadm_id
+order by adult.subject_id;
